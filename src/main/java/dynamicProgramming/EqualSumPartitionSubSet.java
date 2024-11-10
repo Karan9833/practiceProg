ecd546c042702f3fb1a0b4fdb4ff5c27f47c375e
Karan9833 <62711576+Karan9833@users.noreply.github.com>
2024-11-10 23:05:34 +0530

    SumOfSubset problems - check if exists, count , equal partition subset

--- src/main/java/dynamicProgramming/EqualSumPartitionSubSet.java
+++ src/main/java/dynamicProgramming/EqualSumPartitionSubSet.java
@@ -1,4 +1,43 @@
 package main.java.dynamicProgramming;
+/*
+Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
 
+
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+ */
 public class EqualSumPartitionSubSet {
+
+    public boolean checkEqualPartitionSubset(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        return new SumOfSubsets().sumOfSubsetExistTopDownApproach(arr, arr.length, sum / 2);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 11, 5};
+        EqualSumPartitionSubSet equalSumPartitionSubSet = new EqualSumPartitionSubSet();
+        equalSumPartitionSubSet.checkEqualPartitionSubset(arr);
+    }
 }
--- src/main/java/dynamicProgramming/SumOfSubsets.java
+++ src/main/java/dynamicProgramming/SumOfSubsets.java
@@ -1,4 +1,89 @@
 package main.java.dynamicProgramming;
 
+/*
+Given an array arr[] of non-negative integers and a value sum, the task is to check if there is a subset of the given array whose sum is equal to the given sum.
+
+Examples:
+
+Input: arr[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output: True
+Explanation: There is a subset (4, 5) with sum 9.
+
+
+Input: arr[] = {3, 34, 4, 12, 5, 2}, sum = 30
+Output: False
+Explanation: There is no subset that add up to 30.
+ */
 public class SumOfSubsets {
+
+
+    public boolean sumOfSubsetExistTopDownApproach(int arr[], int n, int sum) {
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                //base condition initialization
+                if (j == 0) {
+                    dp[i][j] = true; //1 way in which no of elements(i) can be any but sum (j) =0  i.e empty set
+                }
+                if (i == 0 && j != 0) {
+                    dp[i][j] = false;// 0 ways in which sum ==j but no of elements (i) ==0
+                }
+
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    //j is sum at a given point
+                    //we have two choices if value at arr[i-1] is less than sum (j)
+                    // either include dp[i-1][j-arr[i-1]] or exclude dp[i-1] [j]
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    public int sumOfSubsetCountTopDownApproach(int arr[], int n, int sum) {
+        int dp[][] = new int[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                //base condition initialization
+                if (j == 0) {
+                    dp[i][j] = 1; //1 way in which no of elements(i) can be any but sum (j) =0  i.e empty set
+                }
+                if (i == 0 && j != 0) {
+                    dp[i][j] = 0;// 0 ways in which sum ==j but no of elements (i) ==0
+                }
+
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    //j is sum at a given point
+                    //we have two choices if value at arr[i-1] is less than sum (j)
+                    // either include dp[i-1][j-arr[i-1]] or exclude dp[i-1] [j]
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3, 34, 4, 12, 5, 2};
+        int sum = 9;
+        SumOfSubsets sumOfSubsets = new SumOfSubsets();
+       System.out.println("Sum of subset check if exists : "+ sumOfSubsets.sumOfSubsetExistTopDownApproach(arr, arr.length, sum));
+        System.out.println("Sum of subset pairs count for given array : "+ sumOfSubsets.sumOfSubsetCountTopDownApproach(arr, arr.length, sum));
+        //4,3,2 and 4,5 - two pairs
+
+    }
 }
